507f8e0f2f7606755ffebc25433c93a24272fcd8
Vasiliy Fedorov <fyodorov_vasya@list.ru>
2021-06-07 19:00:02 +0300

    add custom cursor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Background from "./Background/Background";
 import './App.css';
+import Cursor from "./Elements/Cursor/Cursor";
 
 export default function App() {
   return (
     <>
       <Background></Background>
+      <Cursor/>
     </>
   );
 }
---
+++ src/Elements/Cursor/Cursor.js
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import CustomCursor from './CustomCursor';
+import "./Cursor.css";
+
+export default function Cursor() {
+  useEffect(()=> {
+    console.log('Cursor :>> ');
+    const cursor = new CustomCursor(document.querySelector(".cursor"));
+  
+    [...document.querySelectorAll("button")].forEach((el) => {
+      el.addEventListener("mouseenter", () => cursor.emit("enter"));
+      el.addEventListener("mouseleave", () => cursor.emit("leave"));
+    });
+  }, [])
+  return (
+    <>
+      <svg className="cursor" width="90" height="90" viewBox="0 0 90 90">
+        <defs>
+          <filter
+            id="filter-1"
+            x="-50%"
+            y="-50%"
+            width="200%"
+            height="200%"
+            filterUnits="objectBoundingBox"
+          >
+            <feTurbulence
+              type="fractalNoise"
+              baseFrequency="0"
+              numOctaves="10"
+              result="warp"
+            />
+            <feDisplacementMap
+              xChannelSelector="R"
+              yChannelSelector="G"
+              scale="40"
+              in="SourceGraphic"
+              in2="warp"
+            />
+          </filter>
+        </defs>
+        <circle className="cursor__inner" cx="45" cy="45" r="40" />
+      </svg>
+      <div className="cursor__dot"></div>
+    </>
+  );
+}
