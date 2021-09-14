b6d47ee50579e9fbc17fc1b91dbc5edbf3782cdb
Vasiliy Fedorov <fyodorov_vasya@list.ru>
2021-09-14 15:43:54 +0300

    change layout MainBlock

--- src/Blocks/MainBlock/MainBlock.js
+++ src/Blocks/MainBlock/MainBlock.js
@@ -7,25 +7,17 @@ export default function MainBlock() {
   const secondTitleRef = useRef();
   const subtitleRef = useRef();
 
-  const descriptionRef = useRef();
   useEffect(() => {
     surfacingText(firstTitleRef.current, 0);
     surfacingText(secondTitleRef.current, 0);
     surfacingText(subtitleRef.current, 0.5, '-100%');
-    surfacingDescriptionText(descriptionRef.current);
   }, []);
-
-  function surfacingDescriptionText(lines) {
-    lines.childNodes.forEach((line) => {
-      surfacingText(line.firstChild, 1);
-    });
-  }
 
   return (
     <div className={classes.block}>
       <div className={classes.blockContent}>
-        <div className={classes.titleWrap}>
-          <div className={classes.textWrapper}>
+        <div className={classes.textWrapper}>
+          <div className={classes.titleWrap}>
             <div
               ref={firstTitleRef}
               className={classes.titleOutline}
@@ -33,7 +25,7 @@ export default function MainBlock() {
               FRONTEND-
             </div>
           </div>
-          <div className={classes.textWrapper}>
+          <div className={classes.titleWrap}>
             <div
               ref={secondTitleRef}
               className={classes.title}
@@ -41,7 +33,7 @@ export default function MainBlock() {
               DEVELOPER
             </div>
           </div>
-          <div className={classes.textWrapper}>
+          <div className={classes.titleWrap}>
             <div
               ref={subtitleRef}
               className={classes.subtitle}
@@ -50,25 +42,8 @@ export default function MainBlock() {
             </div>
           </div>
         </div>
-        <div
-          ref={descriptionRef}
-          className={classes.description}
-        >
-          <div className={classes.textWrapper}>
-            <div className={classes.descriptionText}>Мне нравится создавать</div>
-          </div>
-          <div className={classes.textWrapper}>
-            <div className={classes.descriptionText}>интерактивные приложения</div>
-          </div>
-          <div className={classes.textWrapper}>
-            <div className={classes.descriptionText}>на JavaScript с акцентом на</div>
-          </div>
-          <div className={classes.textWrapper}>
-            <div className={classes.descriptionText}>анимированный контент,</div>
-          </div>
-          <div className={classes.textWrapper}>
-            <div className={classes.descriptionText}>интерактивные приложения</div>
-          </div>
+        <div className={classes.scrollText}>
+          scroll
         </div>
       </div>
     </div>
