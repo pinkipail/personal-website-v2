729dbd86afb86e5d837a7126a0558b23e80850e4
Vasiliy Fedorov <fyodorov_vasya@list.ru>
2021-09-15 17:52:28 +0300

    revert "refactor SmoothScroll"

--- src/Common/SmoothScroll/SmoothScroll.js
+++ src/Common/SmoothScroll/SmoothScroll.js
@@ -1,60 +1,47 @@
 import React, { useEffect, useRef } from 'react';
-import { gsap, ScrollTrigger } from 'gsap/all';
-import { useWindowSize } from 'react-use';
+import useWindowSize from './hooks/useWindowSize';
 import './SmoothScroll.css';
 
-// TODO: refactoring
-function SmoothScroll({ children }) {
+const data = {
+  ease: 0.1,
+  current: 0,
+  previous: 0,
+  rounded: 0,
+};
+
+const SmoothScroll = ({ children }) => {
   const windowSize = useWindowSize();
-  const parentRef = useRef();
-  const containerRef = useRef();
+  const scrollingContainerRef = useRef();
 
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    setTimeout(() => {
-      handlerWindowSize();
-    }, 100);
-  }, []);
-
-  useEffect(() => {
-    handlerWindowSize();
-    const contentHeight = parentRef.current.scrollHeight - parentRef.current.offsetHeight;
-    animateSmoothScrolling(containerRef.current, contentHeight);
-  }, [windowSize]);
-
-  function handlerWindowSize() {
-    window.scrollTo(0, 0);
-    gsap.killTweensOf(containerRef.current);
-    document.body.style.height = `${parentRef.current.scrollHeight}px`;
-    ScrollTrigger.refresh();
+  function setBodyHeight() {
+    const { height } = scrollingContainerRef.current.getBoundingClientRect();
+    document.body.style.height = `${height}px`;
   }
 
+  function smoothScrollingHandler() {
+    data.current = window.scrollY;
+    data.previous += (data.current - data.previous) * data.ease;
+
+    if (Math.abs(data.current - data.previous) > 1) {
+      scrollingContainerRef.current.style.transform = `translateY(-${Math.round(data.previous)}px)`;
+    }
+
+    requestAnimationFrame(() => smoothScrollingHandler());
+  }
+
+  useEffect(() => {
+    setBodyHeight();
+  }, [windowSize.height]);
+
+  useEffect(() => {
+    requestAnimationFrame(() => smoothScrollingHandler());
+  }, []);
+
   return (
-    <div className="parent" ref={parentRef}>
-      <div ref={containerRef}>{children}</div>
+    <div className="parent">
+      <div ref={scrollingContainerRef}>{children}</div>
     </div>
   );
-}
+};
 
 export default SmoothScroll;
-
-// utils
-
-function animateSmoothScrolling(container, contentHeight) {
-  gsap.fromTo(
-    container,
-    {
-      y: 0,
-    },
-    {
-      scrollTrigger: {
-        trigger: container,
-        scrub: 0.5,
-        start: 'top top',
-        end: 'bottom bottom',
-      },
-      y: -contentHeight,
-      ease: 'none',
-    },
-  );
-}
