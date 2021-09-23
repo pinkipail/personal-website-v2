09633d197b2f560a5b6a6850e14be43101679c60
Vasiliy Fedorov <fyodorov_vasya@list.ru>
2021-09-23 15:31:54 +0300

    add SurfacingTextSymbol

---
+++ src/Common/SurfacingTextSymbol/SurfacingTextSymbol.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useRef } from 'react';
+import { ScrollTrigger } from 'gsap/all';
+import gsap, { Timeline } from 'gsap/gsap-core';
+
+function SurfacingTextSymbol({ children }) {
+  const tl = useRef(new Timeline({ pause: true }));
+  const lettersRef = useRef([]);
+  const containerRef = useRef();
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const scrollTrigger = ScrollTrigger.create({
+      trigger: containerRef.current,
+      toggleActions: 'none play none reset',
+      start: '0% 110%',
+      end: '0% 80%',
+      onEnter: () => tl.current.pause(0),
+      onLeave: () => tl.current.play(),
+    });
+
+    lettersRef.current.forEach((letter, i) => {
+      animateLetter(letter, scrollTrigger, tl.current, i);
+    });
+  }, []);
+
+  return (
+    <span ref={containerRef}>
+      {children.split('').map((letter, i) => (
+        <span
+          ref={(ref) => { lettersRef.current[i] = ref; }}
+          key={i}
+        >
+          {letter}
+        </span>
+      ))}
+    </span>
+  );
+}
+
+export default SurfacingTextSymbol;
+
+// utils
+
+function animateLetter(letter, trigger, timeline, index) {
+  timeline.fromTo(
+    letter,
+    {
+      autoAlpha: 0,
+      y: '0%',
+    },
+    {
+      y: '-50%',
+      autoAlpha: 1,
+      scrollTrigger: trigger,
+      duration: 0.1,
+      delay: index * 0.015,
+    },
+  );
+}
