456ea921869d0f61da21a6b2ac494ca485351d70
Akim <akm_ltw@mail.ru>
2024-02-24 20:03:40 +0300

    complete notification with name query param

--- src/Elements/Notify/Notify.js
+++ src/Elements/Notify/Notify.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import classes from './Notify.module.css';
 
 function Notify() {
+  const [visitorName, setVisitorName] = useState('');
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const nameParam = urlParams.get('name');
+    if (nameParam) {
+      setVisitorName(nameParam);
+    }
+  }, []);
+
+  function onCloseNotification() {
+    setVisitorName('');
+  }
+
   return (
-    <div className={classes.notify}>
-      <div className={classes.notifyBlock}>
-        <button className={classes.closeButton} type="button"><img src="img/icons/close.svg" alt="close" /></button>
-        <span className={classes.title}>Вэлком, Tinkoff!!!</span>
-        <span className={classes.text}>Спасибо, что заглянули ко мне на страничку :)</span>
-      </div>
-    </div>
+    <>
+      { visitorName && (
+        <div className={classes.notify}>
+          <div className={classes.notifyBlock}>
+            <button className={classes.closeButton} type="button" onClick={onCloseNotification}><img src="img/icons/close.svg" alt="close" /></button>
+            <span className={classes.title}>Вэлком, {visitorName}!!!</span>
+            <span className={classes.text}>Спасибо, что заглянули ко мне на страничку :)</span>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
